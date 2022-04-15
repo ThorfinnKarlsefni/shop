b3a41e94d00167efbca7e91886949f588cb135b7
Nick <thorfinnkarlsefni@icloud.com>
2022-04-15 14:47:52 +0800

    user interface - product filters

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -7,10 +7,41 @@ use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $products = Product::query()->where('on_sale',true)->paginate();
+        $builder = Product::query()->where('on_sale',true);
+        //模糊查询
+        if($search = $request->input('search','')){
 
-        return view('products.index',['products' => $products]);
+            $like = '%'.$search.'%';
+
+            $builder->where(function($query) use ($like){
+                $query->where('title','like',$like)
+                    ->orWhere('description','like',$like)
+                    ->orWhereHas('skus',function($query) use ($like){
+                        $query->where('title','like',$like)
+                            ->orWhere('description','like',$like);
+                    });
+            });
+        }
+
+        //排序
+        if($order = $request->input('order','')){
+            if(preg_match('/^(.+)_(asc|desc)$/',$order,$m)){
+                if(in_array($m[1],['price','rating','sold_count'])){
+                    $builder->orderBy($m[1],$m[2]);
+                }
+            }
+        }
+
+        $products = $builder->paginate(16);
+
+        return view('products.index',[
+            'products'  =>  $products,
+            'filters'   =>  [
+                'search'    =>  $search,
+                'order' =>  $order
+            ]
+        ]);
     }
 }
