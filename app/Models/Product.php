5ebd395b661ed1350c50db57571d457dfbf68866
Nick <thorfinnkarlsefni@icloud.com>
2022-04-15 14:15:20 +0800

    user interface - product lists

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Encore\Admin\Traits\DefaultDatetimeFormat;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Product extends Model
 {
@@ -28,5 +29,14 @@ class Product extends Model
     public function skus()
     {
         return $this->hasMany(ProductSku::class);
+    }
+
+    public function getImageUrlAttribute()
+    {
+        if(Str::startsWith($this->attributes['image'],['http://','https://'])){
+            return $this->attributes['image'];
+        };
+
+        return \Storage::disk('public')->url($this->attributes['image']);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/','PagesController@root')->name('root.');
+Route::redirect('/','/products')->name('root.');
 
 Auth::routes(['verify' => true]);
 
@@ -24,4 +24,5 @@ Route::group(['middleware' => ['auth','verified']],function(){
     Route::get('/user_addresses/{user_address}','UserAddressesController@edit')->name('user_addresses.edit');
     Route::put('/user_addresses/{user_address}','UserAddressesController@update')->name('user_addresses.update');
     Route::delete('/user_addresses/{user_address}','UserAddressesController@destroy')->name('user_addresses.destroy');
+    Route::get('/products','ProductsController@index')->name('products.index');
 });
