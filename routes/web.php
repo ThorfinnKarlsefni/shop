a206bb3dc37d10f51c59dbce973b212247835221
Nick <thorfinnkarlsefni@icloud.com>
2022-04-16 17:09:14 +0800

    cart pages

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,9 @@ Route::group(['middleware' => ['auth','verified']],function(){
     Route::get('/products/{product}','ProductsController@show')->name('products.show');
     Route::post('/products/{product}/favorite','ProductsController@favor')->name('products.favor');
     Route::delete('/products/{product}/favorite','ProductsController@disfavor')->name('products.disfavor');
-    Route::post('cart','CartController@add')->name('cart.add');
+    Route::post('/cart','CartController@add')->name('cart.add');
+    Route::get('/cart','CartController@index')->name('cart.index');
+    Route::delete('/cart/{sku}','CartController@remove')->name('cart.remove');
 });
 Route::get('/products','ProductsController@index')->name('products.index');
 Route::get('/products/{product}','ProductsController@show')->name('products.show');
