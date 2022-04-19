e5436db2fd44a0481a2f40a19f625f7f22ea43f4
Nick <thorfinnkarlsefni@icloud.com>
2022-04-19 15:31:17 +0800

    encapsulate business code

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,15 +3,22 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\AddCartRequest;
-use App\Models\CartItem;
 use App\Models\ProductSku;
 use Illuminate\Http\Request;
+use App\Services\CartService;
 
 class CartController extends Controller
 {   
-public function index(Request $request)
+    protected $cartService;
+
+    public function __construct(CartService $cartService)
     {
-        $cartItems = $request->user()->cartItems()->with(['productSku.product'])->get();
+        $this->cartService = $cartService;
+    }
+
+    public function index(Request $request)
+    {
+        $cartItems = $this->cartService->get();
         $address = $request->user()->addresses()->orderBy('last_used_at','desc')->get();
         return view('cart.index',[
             'cartItems' =>  $cartItems,
@@ -21,27 +28,13 @@ public function index(Request $request)
 
     public function add(AddCartRequest $request)
     {
-        $user = $request->user();
-        $amount = $request->input('amount');
-        $skuId = $request->input('sku_id');
-
-        if($cart = $user->cartItems()->where('product_sku_id',$skuId)->first()){
-            $cart->update([
-                'amount' => $cart->amount + $amount
-            ]);
-        }else{
-            $cart = new CartItem(['amount' => $amount]);
-            $cart->user()->associate($user);
-            $cart->productSku()->associate($skuId);
-            $cart->save();
-        }
-
+        $this->cartService->add($request->input('sku_id'),$request->input('amount'));
         return [];
     }
 
     public function remove(ProductSku $sku,Request $request)
     {
-        $request->user()->cartItems()->where('product_sku_id',$sku->id)->delete();
+        $this->cartService->remove($sku->id);
         return [];
     }
 }
